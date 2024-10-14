747caf770b8b73bca4318371ec94ee7acdfdc38c
Rohit <urbanrohita07@gmail.com>
2024-10-14 15:15:38 +0530

    1st commit

--- src/Collection/LargestOfThreeNumbers.java
+++ src/Collection/LargestOfThreeNumbers.java
@@ -6,7 +6,7 @@ public class LargestOfThreeNumbers {
 
 	public static void main(String[] args) {
 		 int x,y,z;
-		 System.out.println("Enter three integers");
+		 System.out.println("Enter three integers value");
 		 Scanner in = new Scanner(System.in);
 		 
 		 x= in.nextInt();
@@ -15,11 +15,11 @@ public class LargestOfThreeNumbers {
 		 
 		 
 		 if (x > y && x > z)
-			 System.out.println("first number is largest.");
+			 System.out.println("first number is largest then the other .");
 		 else if (y > x && y > z)
-			 System.out.println("Second number is largest.");
+			 System.out.println("Second number is largest then the othe .");
 		 else if (z > x && z > y)
-			 System.out.println("Third numner is largest.");
+			 System.out.println("Third numner is largest then the other.");
 		 else
 			 System.out.println("Entered number are not distinct.");
 		 
